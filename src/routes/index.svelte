12b1b17f93238ec586a40a350b575568b3be2007
zeds <tom.zed39@gmail.com>
2022-01-04 17:14:53 +0700

    i18n

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,40 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import translations from './translations'
+
+    import { dict,locale,t } from './i18n'
+
+    $: languages = Object.keys(translations);
+
+    $: dict.set(translations);
+
+</script>
+
+<main>
+    <select bind:value={$locale}>
+        {#each languages as lang}
+            <option value={lang}>
+                {lang}
+            </option>
+        {/each}
+    </select>
+    <!-- <h1>Hello world!</h1> -->
+    <h1>{$t('title.helloworld')}</h1>
+    <h1>{$t('task.workingOnProgress')}</h1>
+
+</main>
+
+<style>
+    main {
+        text-align: center;
+        padding: 1em;
+        max-width: 240px;
+        margin: 0 auto;
+    }
+
+    h1 {
+        color: #ff3e00;
+        text-transform: uppercase;
+        font-size: 4em;
+        font-weight: 100;
+    }
+</style>
---
+++ src/routes/translations.js
@@ -0,0 +1,27 @@
+export default {
+  en: {
+    title: {
+      helloworld: 'Hello world!'
+    },
+    task: {
+      workingOnProgress: 'Working on progress'
+    }
+  },
+  ja: {
+    title: {
+      helloworld: 'こんにちは！'
+    },
+    task: {
+      workingOnProgress: '作業中です..'
+    }
+  },
+  fr: {
+    title: {
+      helloworld: 'Salut monde!'
+    },
+    task: {
+      workingOnProgress: 'En Travail'
+    }
+  },
+
+}
