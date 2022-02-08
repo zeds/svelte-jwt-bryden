e0627fc8b99e0946d28b50da59b14f31ba847cb9
zeds <tom.zed39@gmail.com>
2022-02-09 05:19:11 +0700

    toast

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,10 +1,13 @@
 <script lang="ts">
     import Button from '@smui/button'
+import CloseIcon from '/src/components/CloseIcon.svelte';
+import Contacts from './profile/contacts.svelte';
 </script>
 
-<h1>Homepage</h1>
+<h3>Homepage</h3>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
+<!--<h4>HOST:{import.meta.env.HOST}</h4>-->
 
 
 <Button on:click={() => alert('It worked')}>Click Me!</Button>
